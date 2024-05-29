84a60e62d954d7d8e21918a4c4fb98f0a8ca1c24
Alexeyzem <Alex.zem2015@mail.ru>
2024-05-29 18:07:41 +0300

    update gitignore

---
+++ app/management/commands/generate_cache.py
@@ -0,0 +1,14 @@
+from django.core.cache import cache
+from django.core.management.base import BaseCommand
+
+from app import models
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        best_tags = models.Tag.objects.get_top()
+        cache.set('best_tags', best_tags, 20)
+        print("request in DB for best_tags")
+        best_members = models.Profile.objects.get_top()
+        cache.set('best_members', best_members, 20)
+        print("request in DB for best_members")
